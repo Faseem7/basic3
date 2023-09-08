eb40e20b95eb581dedad3dcc56b78e29829be96f
Faseem7 <76933640+Faseem7@users.noreply.github.com>
2023-09-08 10:04:42 +0530

    starting

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,24 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.red[300],
+          appBar:  AppBar(
+              title: const Text("Attacks"),
+              backgroundColor: Colors.red,
+            ),
+          body: const Dice(),
+          ),
+        ),
+  );
 
   }
+class Dice extends StatelessWidget {
+  const Dice({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
